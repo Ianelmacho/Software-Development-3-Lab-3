e7b8eb0e74fc21d62f0e22e49a24a367de5f4870
Ianelmacho <ian.pullar@icloud.com>
2025-10-16 16:46:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // This will display a message box when the page loads
 alert("✅ JavaScript is working!");
 
-// This writes text inside your HTML paragraph with id="demo"
+// This writes text inside your HTML paragraph with id="demo
 document.getElementById("demo").innerHTML = "Hello, Ian — your code just ran!";
 
 // This also prints a message to the browser's console
